ebde8423c99ac4c927cbb0b764b9d4c595b2b8b2
ArachnidAbby <spidertyler1122@gmail.com>
2026-09-02 21:04:41 -0400

    slight modification

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -96,7 +96,7 @@ def test_get_recursive_includes__pytest():
         "Pygments",
     }
 
-    if sys.platform == "unix":
+    if sys.platform != "win32":
         expected_names.pop("colorama")
 
     assert set(include.name for include in includes) == expected_names
